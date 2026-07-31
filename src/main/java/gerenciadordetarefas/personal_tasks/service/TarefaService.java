e032243a685704072a5a81d9d05a16b8c792b510
paulohscoelho <paulo_henrique006@outlook.com>
2026-07-31 12:35:08 -0300

    atualizando regra de negocio

--- src/main/java/gerenciadordetarefas/personal_tasks/service/TarefaService.java
+++ src/main/java/gerenciadordetarefas/personal_tasks/service/TarefaService.java
@@ -10,27 +10,25 @@ import gerenciadordetarefas.personal_tasks.repository.TarefaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class TarefaService {
-
-
     private final TarefaRepository repository;
-
     private final TarefaMapper mapper;
 
     public TarefaResponseDTO salvarTarefa(TarefaRequestDTO request){
 
         Tarefa tarefa = mapper.paraTarefa(request);
 
-        if(tarefa.getDataFim().isBefore(tarefa.getDataInicio())){
-            throw  new RegraNegocioException(
+        if(tarefa.getDataFim().isBefore(LocalDateTime.now())){
+            throw new RegraNegocioException(
                     "A data de término da tarefa não pode ser anterior a data do inicio da tarefa");
         }
         if (tarefa.getStatus() == Status.CONCLUIDA){
-            throw new RegraNegocioException("A tarefa não pode ser criada como 'CONCLUIDA'");
+            throw new RegraNegocioException("A tarefa não pode ser criada como Status: 'CONCLUIDA'");
         }
 
         Tarefa tarefaSalva = repository.save(tarefa);
@@ -41,18 +39,16 @@ public class TarefaService {
         return mapper.paraResponseDTOList(repository.findAll());
     }
 
-
-    
     public void remover(Long id){
         Tarefa idEncontrado = repository.findById(id)
-                .orElseThrow( ()->new RuntimeException("id nao encontrado") );
+                .orElseThrow( ()-> new RegraNegocioException("id "+id+" não encontrado") );
         repository.delete(idEncontrado);
     }
 
-
+    /// CONTINUAR NA REGRA DE NEGOCIO DO  METODO ATUALIZAR
     public TarefaResponseDTO atualizarPorId(Long id, TarefaRequestDTO taskAntiga){
         Tarefa taskAtualizada = repository.findById(id)
-                .orElseThrow(()-> new RuntimeException("id nao encontrado"));
+                .orElseThrow(()-> new RegraNegocioException("id "+id+" não encontrado"));
         taskAtualizada.setTitulo(taskAntiga.titulo());
         taskAtualizada.setDescricao(taskAntiga.descricao());
         taskAtualizada.setStatus(taskAntiga.status());
@@ -60,12 +56,9 @@ public class TarefaService {
         return mapper.paraResponseDTO( repository.save(taskAtualizada));
     }
 
-
     public TarefaResponseDTO chamarPorId(Long id){
         Tarefa tarefa = repository.findById(id)
-                .orElseThrow(()-> new RuntimeException("id não encontrado"));
+                .orElseThrow(()-> new RegraNegocioException("id "+id+" não encontrado"));
         return mapper.paraResponseDTO(tarefa);
     }
-
-
 }

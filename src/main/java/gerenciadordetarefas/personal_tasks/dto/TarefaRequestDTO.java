9841051a31da94fd08fffbbca25c8c59444ce14d
paulohscoelho <paulo_henrique006@outlook.com>
2026-08-09 11:25:34 -0300

    Minhas alterações nos testes

--- src/main/java/gerenciadordetarefas/personal_tasks/dto/TarefaRequestDTO.java
+++ src/main/java/gerenciadordetarefas/personal_tasks/dto/TarefaRequestDTO.java
@@ -1,15 +1,31 @@
 package gerenciadordetarefas.personal_tasks.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import gerenciadordetarefas.personal_tasks.model.Status;
+import jakarta.validation.constraints.*;
 
 import java.time.LocalDateTime;
 
 public record TarefaRequestDTO(
-        String titulo,
-        String descricao,
-        Status status,
-        LocalDateTime dataInicio,
-        LocalDateTime dataFim
+     @Size(min = 1, max = 100)
+     @NotBlank(message = "título é obrigatório")
+     String titulo,
+
+     @Size(min = 1, max = 250)
+     @NotBlank(message = "descrição é obrigatória")
+     String descricao,
+
+     @NotNull(message = "status é obrigatório")
+     Status status,
+
+     @NotNull(message = "data início é obrigatória")
+     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+     LocalDateTime dataInicio,
+
+
+     @NotNull(message = "data fim é obrigatória")
+     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+     LocalDateTime dataFim
 ) {
 
 }
---
+++ src/test/java/gerenciadordetarefas/personal_tasks/controller/TarefaControllerTest.java
@@ -0,0 +1,7 @@
+package gerenciadordetarefas.personal_tasks.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TarefaControllerTest {
+
+}
